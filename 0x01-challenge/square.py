8550621a97e076434965399e178c36ea042a4d20
Amira Walid <amirawaleed012@gmail.com>
2024-05-06 14:13:30 +0300

    fix square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
+""" Square module. """
+
 
 class square():
-
+    """ Square class. """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Square class initalize. """
         for key, value in kwargs.items():
             setattr(self, key, value)
-        self.height = self.width
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """ Area of the square. """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """ Permiter of the square. """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation of square. """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
